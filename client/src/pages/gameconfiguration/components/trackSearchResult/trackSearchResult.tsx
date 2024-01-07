05a04702dac5ffa2e651cd0ce494fa52cfc2bb5c
Colty1999 <mateuszgietka99@gmail.com>
2024-01-07 18:46:46 +0100

    spotify playlist description truncation added

--- client/src/pages/gameconfiguration/components/trackSearchResult/trackSearchResult.tsx
+++ client/src/pages/gameconfiguration/components/trackSearchResult/trackSearchResult.tsx
@@ -5,18 +5,30 @@ import { AppContext } from "../../../../App";
 
 interface TrackSearchResultProps {
     track: {
-        artist: string,
         title: string,
+        description: string,
+
         uri: string,
         albumUrl?: string,
     }
 }
 
 const TrackSearchResult = ({ track }: TrackSearchResultProps) => {
-    const { artist, title, uri, albumUrl } = track;
+    const { title, description, uri, albumUrl } = track;
     let currentSongUri = useStorageState({ state: "currentSongUri" });
     let { songPlaying, setSongPlaying } = useContext(AppContext);
     const [songActive, setSongActive] = useState<boolean>(false);
+
+    const truncateString = (str: string, maxLength: number) => {
+        if (str.length <= maxLength) {
+            return str;
+        } else {
+            const lastSpaceIndex = str.lastIndexOf(' ', maxLength);
+            return lastSpaceIndex !== -1 ? str.substring(0, lastSpaceIndex) + '...' : str.substring(0, maxLength) + '...';
+        }
+    };
+    const maxLength = 90;
+    const truncatedString = truncateString(description, maxLength);
 
     const onClick = () => {
         currentSongUri.setStorageState(uri);
@@ -40,7 +52,7 @@ const TrackSearchResult = ({ track }: TrackSearchResultProps) => {
                 <img src={albumUrl} alt={track.title} />
                 <div className="songData">
                     <h3>{title}</h3>
-                    <h4>{artist}</h4>
+                    <h4>{truncatedString}</h4>
                 </div>
             </div>
         </div>
