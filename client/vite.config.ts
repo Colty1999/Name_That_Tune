2be5fad7dcbc21aa23699343ddda33befebb78cd
Colty1999 <mateuszgietka99@gmail.com>
2024-02-26 16:35:36 +0100

    frontend base url cleared

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.REACT_APP_BASE_URL,
+  // base: process.env.REACT_APP_BASE_URL,
 })
