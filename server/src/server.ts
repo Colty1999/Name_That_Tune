404985896d46c39592c267d0fe058bbcfa9ba9d4
Colty1999 <mateuszgietka99@gmail.com>
2024-02-26 16:51:10 +0100

    next redirect fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 // const dotenv = require('dotenv');
 dotenv.config();
 
-const frontend = "https://" + process.env.FRONTEND_URI;
+const frontend = process.env.FRONTEND_URI;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
