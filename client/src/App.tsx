57c590433c3e970110b10b24ab28764957e5ee10
Mateusz Gietka <mateuszgietka99@gmail.com>
2024-12-18 02:45:48 +0100

    netlify redirect app added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,68 +1,24 @@
-import MainMenu from './pages/mainmenu/mainMenu'
-import Container from 'react-bootstrap/esm/Container';
-// import DemoGame from './pages/demogame/demoGame';
-// import DemoGameMaster from './pages/demogamemaster/demoGameMaster';
-import NotFound from './pages/notfound/notFound';
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
-import NotImplemented from './pages/notimplemented/notImplemented';
-import Header from './components/header/header';
-import { createContext, useState } from 'react';
-import Loader from './components/loader/loader';
-import GameConfiguration from './pages/gameconfiguration/gameConfiguration';
-import useAuth from './hooks/useAuth';
-import SpotifyGameMaster from './pages/spotifygamemaster/spotifyGameMaster';
-import SpotifyGame from './pages/spotifygame/spotifyGame';
-import Instruction from './pages/insctruction/instruction';
-import ErrorModal from './components/errorModal/errorModal';
+import { useEffect } from "react";
+import Loader from "./loader/loader";
 
-export const AppContext = createContext<{
-  setLoading: (React.Dispatch<React.SetStateAction<boolean>> | Function),
-  token: (string | null),
-  setToken: (React.Dispatch<React.SetStateAction<string | null>> | Function),
-  songPlaying: boolean,
-  setSongPlaying: (React.Dispatch<React.SetStateAction<boolean>> | Function),
-  playerLoaded: boolean,
-  setPlayerLoaded: (React.Dispatch<React.SetStateAction<boolean>> | Function),
-  error: string,
-  setError: (React.Dispatch<React.SetStateAction<string>> | Function)
-}>({ setLoading: () => { }, token: null, setToken: () => { }, songPlaying: false, setSongPlaying: () => { }, playerLoaded: false, setPlayerLoaded: () => { }, error: "", setError: () => { }});
+const gameLogo = "./gameLogo.png";
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [token, setToken] = useState<string | null>(new URLSearchParams(window.location.search).get('code'));
-  const [songPlaying, setSongPlaying] = useState<boolean>(false);
-  const [playerLoaded, setPlayerLoaded] = useState<boolean>(false);
-  const [error, setError] = useState<string>("");
-  useAuth(token);
+
+  useEffect(() => {
+    // Redirect to the desired URL
+    window.location.href = "https://namethattune.vercel.app/";
+  }, []);
 
   return (
-    <Container fluid="true" className='container'>
-      <HashRouter >
-        <AppContext.Provider value={{ setLoading, token, setToken, songPlaying, setSongPlaying, playerLoaded, setPlayerLoaded, error, setError }}>
-          {loading && <Loader />}
-          <ErrorModal />
-          <Header />
-          <Routes>
-            <Route path="/" Component={MainMenu} />
-            {/* <Route path="/demogame" Component={DemoGame} />
-            <Route path="/demogamemaster" Component={DemoGameMaster} /> */}
-            <Route path="/spotifygame" Component={SpotifyGame} />
-            <Route path="/spotifygamemaster" Component={SpotifyGameMaster} />
-            <Route path="/gameconfiguration" Component={GameConfiguration} />
-            <Route path="instruction" Component={Instruction} />
-            {/* <Route path="/game" Component={GalleryYearView} />
-            <Route path="/archive/:year/:folder" Component={GalleryFolderView} />*/}
-            <Route path='/notimplemented' Component={NotImplemented} />
-            <Route path='/404' Component={NotFound} />
-            <Route
-            path="*"
-            element={<Navigate to="/404" replace />}
-          />
-          </Routes>
-        </AppContext.Provider>
-      </HashRouter >
-    </Container>
-  )
-}
+    <main className="mainmenu">
+            <div>
+                <img src={gameLogo} className="logo" alt="logo" />
+            </div>
+      <h3 style={{color: "#646cff", paddingTop: "1rem"}}>Redirecting...</h3>
+      <Loader />
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/loader/loader.tsx
@@ -0,0 +1,17 @@
+import "./loader.scss"
+
+interface LoaderProps {
+    style?: React.CSSProperties;
+}
+
+const Loader = (props: LoaderProps) => {
+    return (
+        <div className="loaderoutercontainer" style={props.style}>
+            <div className="loaderinnercontainer">
+                <div className="loader" />
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
