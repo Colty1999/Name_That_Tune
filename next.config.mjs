4385fde01980c1bec3198a12ea5fd51fb16c171d
Mateusz Gietka <mateuszgietka99@gmail.com>
2024-12-18 02:13:34 +0100

    output dir fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     // output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: './dist', // Changes the build output directory to `./dist/`.
+    // distDir: './dist', // Changes the build output directory to `./dist/`.
     env: {
       NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
     }
